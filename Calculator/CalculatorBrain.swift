6d2aaf50b7b68b3c7024d622b7828d43f5a656b2
mnipper <mjn4406@gmail.com>
2015-04-21 23:32:10 -0400

    Create CalculatorBrain model

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,70 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Michael Nipper on 4/19/15.
+//  Copyright (c) 2015 Michael Nipper. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain
+{
+    private enum Op {
+        case Operand(Double)
+        case UnaryOperation(String, Double -> Double)
+        case BinaryOperation(String, (Double, Double) -> Double)
+    }
+    
+    private var opStack = [Op]()
+    private var knownOps = [String:Op]()
+    
+    init() {
+        knownOps["*"] = Op.BinaryOperation("*", *)
+        knownOps["/"] = Op.BinaryOperation("/") { $1 / $0 }
+        knownOps["+"] = Op.BinaryOperation("+", +)
+        knownOps["-"] = Op.BinaryOperation("-") { $1 - $0 }
+        knownOps["sqrt"] = Op.UnaryOperation("sqrt", sqrt)
+    }
+    
+    private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op]) {
+        if !ops.isEmpty {
+            var remainingOps = ops
+            let op = remainingOps.removeLast()
+            switch op {
+            case Op.Operand(let operand):
+                return (operand, remainingOps)
+            case Op.UnaryOperation(_, let operation):
+                let operandEvaluation = evaluate(remainingOps)
+                if let operand = operandEvaluation.result {
+                    return (operation(operand), operandEvaluation.remainingOps)
+                }
+            case Op.BinaryOperation(_, let operation):
+                let op1Evaluation = evaluate(remainingOps)
+                if let op1 = op1Evaluation.result {
+                    let op2Evaluation = evaluate(op1Evaluation.remainingOps)
+                    if let op2 = op2Evaluation.result {
+                        return (operation(op1, op2), op2Evaluation.remainingOps)
+                    }
+                }
+            }
+        }
+        
+        return (nil, ops)
+    }
+    
+    func evaluate() -> Double? {
+        let (result, remainingOps) = evaluate(opStack)
+        return result
+    }
+    
+    func pushOperand(operand: Double) {
+        opStack.append(Op.Operand(operand))
+    }
+    
+    func performOperation(symbol: String) {
+        if let operation = knownOps[symbol] {
+            opStack.append(operation)
+        }
+    }
+}
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,64 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    var userHasPressedButton = false
+    var operandStack = Array<Double>()
+    
+    @IBAction func enter() {
+        userHasPressedButton = false
+        operandStack.append(displayValue)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func numberButton(sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if userHasPressedButton {
+            display.text = display.text! + digit
+        } else {
+             display.text = digit
+             userHasPressedButton = true
+        }
     }
-
-
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userHasPressedButton {
+            enter()
+        }
+        switch operation {
+        case "x": performOperation { $0 * $1 }
+        case "/": performOperation { $1 / $0 }
+        case "+": performOperation { $0 + $1 }
+        case "-": performOperation { $1 - $0 }
+        case "sq": performOperation { sqrt($0) }
+        default: break
+        }
+    }
+    
+    func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    func performOperation(operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userHasPressedButton = false
+        }
+    }
 }
 
